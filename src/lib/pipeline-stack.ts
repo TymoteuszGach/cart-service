42d8882703a8b3ad810f3a653bdb829993e93068
Tymoteusz Gach <tymoteusz.gach@merapar.com>
2022-11-17 00:56:18 +0100

    fix: update pipelines to use CodeStar connection

--- src/lib/pipeline-stack.ts
+++ src/lib/pipeline-stack.ts
@@ -1,4 +1,5 @@
 import { Stack, StackProps } from "aws-cdk-lib";
+import * as ssm from "aws-cdk-lib/aws-ssm";
 import { CodePipeline, CodePipelineSource, ShellStep } from "aws-cdk-lib/pipelines";
 import { Construct } from "constructs";
 import { PipelineAppStage, PipelineAppStageProps } from "./pipeline-app-stage";
@@ -6,6 +7,8 @@ import { PipelineAppStage, PipelineAppStageProps } from "./pipeline-app-stage";
 const DEFAULT_MAIN_BRANCH_NAME = "main";
 
 export interface GitProps {
+  codeStarConnectionSSMParameterName: string;
+  owner: string;
   repository: string;
   branch?: string;
 }
@@ -19,12 +22,19 @@ export class PipelineStack extends Stack {
   constructor(scope: Construct, id: string, props: PipelineStackProps) {
     super(scope, id, props);
 
+    const connectionArn = ssm.StringParameter.fromStringParameterAttributes(this, "ConnectionParameter", {
+      parameterName: props.git.codeStarConnectionSSMParameterName,
+    }).stringValue;
+
+    const repositoryName = `${props.git.owner}/${props.git.repository}`;
     const branch = props.git.branch ?? DEFAULT_MAIN_BRANCH_NAME;
 
     const pipeline = new CodePipeline(this, "Pipeline", {
       synth: new ShellStep("Synth", {
-        input: CodePipelineSource.gitHub(props.git.repository, branch),
-        commands: ["npm run test", "npm run package"],
+        input: CodePipelineSource.connection(repositoryName, branch, {
+          connectionArn: connectionArn,
+        }),
+        commands: ["npm run build", "npm run synth"],
       }),
     });
 
--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,15 @@ import "source-map-support/register";
 import * as cdk from "aws-cdk-lib";
 import { PipelineStack, PipelineStackProps } from "./lib/pipeline-stack";
 
-const REPOSITORY_NAME = "tymoteuszgach/cart-service";
+const REPOSITORY_OWNER = "TymoteuszGach";
+const REPOSITORY_NAME = "cart-service";
 
 const app = new cdk.App();
 const props: PipelineStackProps = {
   git: {
+    owner: REPOSITORY_OWNER,
     repository: REPOSITORY_NAME,
+    codeStarConnectionSSMParameterName: "/github-connection-arn",
   },
   pipelineAppStageProps: {},
 };
